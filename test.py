12616799dc54448cc970c3f0e99669e459a620bf
vaishali <vaishali.dhote=@gammaedge.io>
2024-10-08 17:15:38 +0530

    Update by v

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-import pytest
 import app
-def test_add(x,y):
-    assert app.add(2,3)==5
-    assert app.add(1,2)==3
 
-def test_subtract(a,b):
-    assert app.subtract(3,0)==3
-    assert app.subtract(10,5)==5
+# import pytest
 
+
+def test_add(x, y):
+    assert app.add(2, 3) == 5
+    assert app.add(1, 2) == 3
+
+
+def test_subtract(a, b):
+    assert app.subtract(3, 0) == 3
+    assert app.subtract(10, 5) == 5
